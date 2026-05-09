ef124834b3c5e09d9e3bc9c7e6442121296b832a
sohamghoshey <soham.ghosh@in.ey.com>
2026-05-09 17:34:29 +0530

    mock payment gateway success

--- micro-ui/web/micro-ui-internals/packages/modules/common/src/payments/citizen/payment-type/index.js
+++ micro-ui/web/micro-ui-internals/packages/modules/common/src/payments/citizen/payment-type/index.js
@@ -94,7 +94,7 @@ export const SelectPaymentType = (props) => {
     try {
       const data = await Digit.PaymentService.createCitizenReciept(billDetails?.tenantId, filterData);
       const redirectUrl = data?.Transaction?.redirectUrl;
-      if (d?.paymentType == "AXIS") {
+      if (d?.paymentType == "AXIS" || d?.paymentType == "MOCK") {
         window.location = redirectUrl;
       }
       else if (d?.paymentType == "NTTDATA") {
